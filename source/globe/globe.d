c16c9b47c1e490d429900e899e948d23918b6a52
Anders Andersson <trezker@gmail.com>
2015-02-05 20:09:54 +0100

    Converting coords in globe to pointdata struct

--- source/globe/globe.d
+++ source/globe/globe.d
@@ -21,9 +21,19 @@ import alledged.model;
  * which must always be the same no matter where you go into a detailed map.
  * */
 
+struct Pointdata {
+	vec3 coord;
+	float height;
+};
+
 class Edge {
 public:
+/*
 	this(vec3[2] c) {
+		coords[] = c;
+	}
+*/
+	this(Pointdata[2] c) {
 		coords[] = c;
 	}
 	
@@ -47,46 +57,47 @@ public:
 
 	Edge[2] Get_children() {
 		if(children[0] is null) {
-			vec3 mid = coords[0] + coords[1];
+			vec3 mid = coords[0].coord + coords[1].coord;
 			mid.normalize();
-			children[0] = new Edge([coords[0], mid]);
-			children[1] = new Edge([coords[1], mid]);
+			Pointdata midpoint = Pointdata(mid, 0);
+			children[0] = new Edge([coords[0], midpoint]);
+			children[1] = new Edge([coords[1], midpoint]);
 		}
 		return children[0 .. 2];
 	}
 	
-	vec3[2] Get_coords() {
+	Pointdata[2] Get_coords() {
 		return coords[0 .. 2];
 	}
 	
 	vec3 Get_nearest_point(vec3 center) {
-		auto v = coords[1] - coords[0];
-		auto w = center - coords[0];
+		auto v = coords[1].coord - coords[0].coord;
+		auto w = center - coords[0].coord;
 
-		auto c1 = dot(w, v);		
+		auto c1 = dot(w, v);
 		if (c1 <= 0) {
-			return coords[0];
+			return coords[0].coord;
 		}
 		
 		auto c2 = dot(v, v);
 		if (c2 <= c1) {
-			return coords[1];
+			return coords[1].coord;
 		}
 
 		auto b = c1 / c2;
-		auto Pb = coords[0] + b * v;
+		auto Pb = coords[0].coord + b * v;
 		return Pb;
 	}
 private:
 	Globe globe;
-	vec3[2] coords;
+	Pointdata[2] coords;
 	Edge[2] children;
 	Face[2] faces;
 };
 
 class Face {
 public:
-	this(int l, vec3[3] c, Edge[3] e) {
+	this(int l, Pointdata[3] c, Edge[3] e) {
 		coords[] = c;
 		edges[] = e;
 		level = l;
@@ -95,7 +106,7 @@ public:
 	Face[] Get_children() {
 		if(children[0] is null) {
 			Edge[] childedges;
-			vec3[] midpoints;
+			Pointdata[] midpoints;
 			foreach(i, edge; edges) {
 				//Put the edges in a known order here.
 				Edge[2] edgepair = edge.Get_children();
@@ -115,7 +126,7 @@ public:
 			childedges ~= new Edge([midpoints[2], midpoints[0]]);
 			
 			
-			vec3[3] facecoords = [coords[0], midpoints[0], midpoints[2]];
+			Pointdata[3] facecoords = [coords[0], midpoints[0], midpoints[2]];
 			Edge[3] faceedges = [childedges[0], childedges[8], childedges[5]];
 			Face f = new Face(level+1, facecoords, faceedges);
 			foreach(edge; faceedges) {
@@ -164,7 +175,7 @@ public:
 	vec3 Get_center() {
 		vec3 c = vec3(0, 0, 0);
 		foreach(v; coords) {
-			c += v;
+			c += v.coord;
 		}
 		c.x /= 3;
 		c.y /= 3;
@@ -172,12 +183,12 @@ public:
 		return c;
 	}
 	
-	vec3[3] Get_coords() {
+	Pointdata[3] Get_coords() {
 		return coords[0 .. 3];
 	}
 private:
 	Globe globe;
-	vec3[3] coords;
+	Pointdata[3] coords;
 	Edge[3] edges;
 	Face[4] children;
 	int level;
@@ -187,6 +198,7 @@ class Globe {
 public:
 	void Init(float r) {
 		radius = r;
+		/*
 		vec3[] coords = [
 			vec3(0, r, 0),
 			vec3(r, 0, 0),
@@ -194,6 +206,16 @@ public:
 			vec3(0, -r, 0),
 			vec3(-r, 0, 0),
 			vec3(0, 0, -r),
+		];
+		*/
+		
+		Pointdata[] coords = [
+			Pointdata(vec3(0, r, 0), 0),
+			Pointdata(vec3(r, 0, 0), 0),
+			Pointdata(vec3(0, 0, r), 0),
+			Pointdata(vec3(0, -r, 0), 0),
+			Pointdata(vec3(-r, 0, 0), 0),
+			Pointdata(vec3(0, 0, -r), 0),
 		];
 		
 		Edge[] edges = [
@@ -228,7 +250,7 @@ public:
 		
 		
 		for(int i = 0; i < facelist.length; i+= 6) {
-			vec3[3] facecoords = [coords[facelist[i]], coords[facelist[i+1]], coords[facelist[i+2]]];
+			Pointdata[3] facecoords = [coords[facelist[i]], coords[facelist[i+1]], coords[facelist[i+2]]];
 			Edge[3] faceedges = [edges[facelist[i+3]], edges[facelist[i+4]], edges[facelist[i+5]]];
 
 			Face f = new Face(1, facecoords, faceedges);
@@ -284,20 +306,24 @@ public:
 		}
 		
 		//Create model
-		vec3[] model_coords;
+		Pointdata[] model_points;
 		int[] model_faces;
 		foreach(face; included_faces) {
 			auto corners = face.Get_coords();
 			foreach(corner; corners) {
-				int p = to!int(countUntil(model_coords, corner));
+				int p = to!int(countUntil(model_points, corner));
 				if(p == -1) {
-					model_coords ~= corner;
-					model_faces ~= to!int(model_coords.length - 1);
+					model_points ~= corner;
+					model_faces ~= to!int(model_points.length - 1);
 				}
 				else {
 					model_faces ~= p;
 				}
 			}
+		}
+		vec3[] model_coords;
+		foreach(point; model_points) {
+			model_coords ~= point.coord;
 		}
 		Model model = new Model;
 		model.Set_model_data(model_coords, model_faces);
